c82216190ca6ccf88416efb4473adedafad68676
Lord Hung <hung.tran@systum.com>
2019-03-06 14:41:41 +0700

    add machine ip

--- backend/src/settings/development.py
+++ backend/src/settings/development.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.99.100']
 
 
 # Database
